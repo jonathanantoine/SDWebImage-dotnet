0f195f68fa5dfb81cb904ea894afcc6b3dc7c801
Jonathan ANTOINE <antoine_jonathan@hotmail.com>
2025-04-16 11:21:59 +0200

    Add linking options for arm64 + frameworks

---
+++ libSDWebImage.linkwith.cs
@@ -0,0 +1,4 @@
+﻿using ObjCRuntime;
+
+[assembly: LinkWith ("libSDWebImage.a", LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, 
+    LinkerFlags = "-ObjC -fobjc-arc", Frameworks = "CoreGraphics ImageIO QuartzCore", SmartLink = true, ForceLoad = true)]
