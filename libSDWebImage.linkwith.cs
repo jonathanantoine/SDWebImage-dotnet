8ccf6bf9070b4fd7a81df34890a9c899aac2af5f
Jonathan ANTOINE <antoine_jonathan@hotmail.com>
2025-04-16 11:46:57 +0200

    attempt to fix  Undefined symbols for architecture arm64

--- libSDWebImage.linkwith.cs
+++ libSDWebImage.linkwith.cs
@@ -1,4 +1,5 @@
 ﻿using ObjCRuntime;
 
 [assembly: LinkWith ("libSDWebImage.a", LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, 
-    LinkerFlags = "-ObjC -fobjc-arc", Frameworks = "CoreGraphics ImageIO QuartzCore", SmartLink = true, ForceLoad = true)]
+    LinkerFlags = "-ObjC -fobjc-arc", Frameworks = "CoreGraphics ImageIO QuartzCore Foundation", SmartLink = true, ForceLoad = true)]
+    
