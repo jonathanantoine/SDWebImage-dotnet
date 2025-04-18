7ddc5e7acf1b5ed987fefec8482901ace0ee02a5
Jonathan ANTOINE <antoine_jonathan@hotmail.com>
2025-04-18 20:51:53 +0200

    Fix link me attribute

--- libSDWebImage.linkwith.cs
+++ libSDWebImage.linkwith.cs
@@ -1,5 +1,5 @@
 ﻿using ObjCRuntime;
 
-[assembly: LinkWith ("libSDWebImage.a", LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, 
+[assembly: LinkWith ("SDWebImage.xcframework", LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, 
     LinkerFlags = "-ObjC -fobjc-arc", Frameworks = "CoreGraphics ImageIO QuartzCore Foundation", SmartLink = true, ForceLoad = true, WeakFrameworks = "MapKit")]
     
