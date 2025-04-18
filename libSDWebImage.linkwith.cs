7055d33d6e1eb68fd8aead0f6f7f736c9054447f
Jonathan ANTOINE <antoine_jonathan@hotmail.com>
2025-04-18 20:53:40 +0200

    remove useless file

--- libSDWebImage.linkwith.cs
+++
@@ -1,5 +0,0 @@
-﻿using ObjCRuntime;
-
-[assembly: LinkWith ("SDWebImage.xcframework", LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, 
-    LinkerFlags = "-ObjC -fobjc-arc", Frameworks = "CoreGraphics ImageIO QuartzCore Foundation", SmartLink = true, ForceLoad = true, WeakFrameworks = "MapKit")]
-    
